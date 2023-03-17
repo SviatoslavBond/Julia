686a2628401a275863176f05bca028a6dcfd08a7
sviat <sviat@admins-MacBook-Pro.local>
2023-03-17 22:08:53 +0200

    changed token

--- src/utils/fetchInstaData.js
+++ src/utils/fetchInstaData.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const TOKEN = 'IGQVJXdGc2T2JwMkI1dTJaSHE1NVFnUk1udzFVcmJMNjRtc2ZASVjIxRlprSXVMYTNWOU9nT0x2bm5QamhXT1EybkJwRUNyQXVPVU1ielV0UmU5WmxwVm15blBHN1BnX0taTWE2RWZA3';
+const TOKEN = 'IGQVJYT2hjdmdqOGh6ekMxMzlvZAk1tTFZAkeWNSeEVVY01tSUtIVWZAZAdllmaVVfakM3b2pqWmVjWDFKTFNqZAVlieXVBRFVfVXMwYnVsN2RRY2pRMUlBenIyTjRPNUl4NVdpa3lmWFR2bWRqbmtYVXZAKWgZDZD';
 const baseEndPoint = "https://graph.instagram.com";
 const fields = 'id,media_type,media_url,timestamp,thumbnail_url,permalink,caption';
 const limit = 9;
