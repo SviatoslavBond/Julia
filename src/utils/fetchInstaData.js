b1e034a27e3c602be83a63362d600a9162ab969c
Sviatoslav Bondarenko <103562674+SviatoslavBond@users.noreply.github.com>
2023-03-18 14:07:49 +0200

    Update fetchInstaData.js

--- src/utils/fetchInstaData.js
+++ src/utils/fetchInstaData.js
@@ -7,7 +7,7 @@ const limit = 9;
 
 export const fetchInstaData = async () => {
 
-	const data = await axios.get(`${baseEndPoint}/me/media?fields=${fields}&limit=${limit}&access_token=${process.env.SECRET_KEY}`)
+	const data = await axios.get(`${baseEndPoint}/me/media?fields=${fields}&limit=${limit}&access_token=${process.env.REACT_APP_INSTA_KEY}`)
 	return data;
 
 }
